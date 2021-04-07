7985e7507bdbfd7340f9628956e98788446ffc7a
P V Tharun <79517306+PVTHARUN@users.noreply.github.com>
2021-04-07 22:01:11 +0530

    src

---
+++ src/data.js
@@ -0,0 +1,12 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.projectData = [
+    { 
+        TaskID: 1, TaskName: 'Planning', StartTime: new Date('04/01/2021'), EndTime: new Date('04/05/2021'), status: 'Finished', Duration: 5, Progress: 100 
+    },
+    { 
+        TaskID: 2, TaskName: 'Preparation', StartTime: new Date('04/05/2021'), EndTime: new Date('04/06/2021'), status: 'InProgress', Duration: 1, Progress: 50 
+    },
+    { 
+        TaskID: 3, TaskName: 'Procurement', StartTime: new Date('04/06/2021'), EndTime: new Date('04/10/2021'), status: 'YetToStart', Duration: 5, Progress: 0 
+    },
+]; 
